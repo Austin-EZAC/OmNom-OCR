3bf241b3d9ba34df5300a90c1b19f2d1a4935dd8
Chris Large <christopher.large@ezautomationconsulting.com>
2021-09-30 19:10:10 -0500

    Add S3 Buckets to Scripts (#7)

--- omnom/omnom/omnom_stack.py
+++ omnom/omnom/omnom_stack.py
@@ -1,6 +1,7 @@
 import aws_cdk.core as cdk
 import aws_cdk.aws_sns as sns
 import aws_cdk.aws_iam as iam
+import aws_cdk.aws_s3 as s3
 
 class OmnomStack(cdk.Stack):
 
@@ -19,6 +20,22 @@ class OmnomStack(cdk.Stack):
             effect = iam.Effect.ALLOW,
             resources = [jobCompletionTopic.topic_arn],
             actions = ["sns:Publish"]))
+
+        #**********S3 Batch Operations Role******************************
+        s3BatchOperationsRole = iam.Role(self, 'S3BatchOperationsRole', assumed_by=iam.ServicePrincipal('batchoperations.s3.amazonaws.com'))
+
+
+        # **********S3 Bucket******************************
+        # S3 bucket for input documents and output
+        contentBucket = s3.Bucket(self, 'DocumentsBucket', versioned= False, auto_delete_objects = True, removal_policy = cdk.RemovalPolicy.DESTROY)
+        
+        existingContentBucket = s3.Bucket(self, 'ExistingDocumentsBucket', versioned= False, auto_delete_objects = True, removal_policy = cdk.RemovalPolicy.DESTROY)
+        existingContentBucket.grant_read_write(s3BatchOperationsRole)
+
+        inventoryAndLogsBucket = s3.Bucket(self, 'InventoryAndLogsBucket', versioned= False, auto_delete_objects = True, removal_policy = cdk.RemovalPolicy.DESTROY)
+        inventoryAndLogsBucket.grant_read_write(s3BatchOperationsRole)
+
+        
 
         
 
